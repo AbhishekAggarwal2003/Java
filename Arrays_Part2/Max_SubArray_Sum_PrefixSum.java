42e1324ad80af5cb5474301b2e3b30a257e9bb90
AbhishekAggarwal2003 <abhiaggarwal171114@gmail.com>
2024-01-22 01:20:40 +0530

    Arrays_Part2

--- Arrays_Part2/Max_SubArray_Sum_PrefixSum.java
+++ Arrays_Part2/Max_SubArray_Sum_PrefixSum.java
@@ -1,17 +1,18 @@
 package Arrays_Part2;
 
-public class Max_SubArray_Sum_BruteForce {
+public class Max_SubArray_Sum_PrefixSum {
+    
     public static void maxsubarraysum(int array[]){
         int max_sum=Integer.MIN_VALUE;
+        int prefix[]=new int[array.length];
+        prefix[0]=array[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+array[i];
+        }
+        //  i==start    j==end
         for(int i=0;i<array.length;i++){
             for(int j=i;j<array.length;j++){
-                int sum=0;
-                for(int k=i;k<=j;k++){
-                    System.out.print(array[k]+" ");
-                    sum=sum+array[k];
-                }
-                System.out.print("  Sum:"+sum);
-                System.out.println();
+                int sum = i==0? prefix[j] : prefix[j]-prefix[i-1]; //ternary operator
                 if(sum>max_sum){
                     max_sum=sum;
                 }
